eb43cab0e2cce5ae4f8bf4f0fb97f98a24c2d0e6
eruiz <eruiz2706@gmail.com>
2019-09-08 11:31:40 -0500

    ajuste migracion

--- resources/views/layouts/adminlte/partials/htmlheader.blade.php
+++ resources/views/layouts/adminlte/partials/htmlheader.blade.php
@@ -9,20 +9,21 @@
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">
   
+  <!--<link rel="stylesheet" href="{{ asset('plugins/ionicons/css/ionicons.min.css') }}">-->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link rel="stylesheet" href="http://localhost/votantes/public/plugins/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="{{ asset('plugins/ionicons/css/ionicons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/social/social.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/select2/select2.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/toastr/toastr.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/sweetalert/sweetalert.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar/fullcalendar.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar/fullcalendar.print.css') }}" media="print">
-  <link rel="stylesheet" href="{{ asset('rsc/css/adminlte.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('rsc/css/loader-1.css') }}">
-  <link rel="stylesheet" href="{{ asset('rsc/css/style.css') }}">
-  <link rel="stylesheet" href="{{ asset('rsc/css/courses.css') }}">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/social/social.css">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/select2/select2.min.css">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/toastr/toastr.min.css">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/sweetalert/sweetalert.css">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/fullcalendar/fullcalendar.min.css">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/fullcalendar/fullcalendar.print.css" media="print">
+  <link rel="stylesheet" href="http://localhost/votantes/rsc/css/adminlte.min.css">
+  <link rel="stylesheet" href="http://localhost/votantes/rsc/css/loader-1.css">
+  <link rel="stylesheet" href="http://localhost/votantes/rsc/css/style.css">
+  <link rel="stylesheet" href="http://localhost/votantes/rsc/css/courses.css">
 
-  <link rel="stylesheet" href="{{ asset('plugins/morris/morris.css') }}">
+  <link rel="stylesheet" href="http://localhost/votantes/plugins/morris/morris.css">
 </head>
   
--- resources/views/layouts/adminlte/partials/scripts.blade.php
+++ resources/views/layouts/adminlte/partials/scripts.blade.php
@@ -1,24 +1,24 @@
 
-<script src="{{ asset('plugins/jquery/jquery1.9.min.js') }}"></script>
-<script src="{{ asset('plugins/morris/raphael-min.js') }}"></script>
-<script src="{{ asset('plugins/morris/morris.min.js') }}"></script>
-<script src="{{ asset('plugins/morris/morris-horizontal.min.js') }}"></script>
+<script src="http://localhost/votantes/plugins/jquery/jquery1.9.min.js"></script>
+<script src="http://localhost/votantes/plugins/morris/raphael-min.js"></script>
+<script src="http://localhost/votantes/plugins/morris/morris.min.js"></script>
+<script src="http://localhost/votantes/plugins/morris/morris-horizontal.min.js"></script>
 
-<script src="{{ asset('plugins/select2/select2.full.min.js') }}"></script>
-<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
-<script src="{{ asset('plugins/fastclick/fastclick.js') }}"></script>
-<script src="{{ asset('plugins/toastr/toastr.js') }}"></script>
-<script src="{{ asset('plugins/sweetalert/sweetalert.min.js') }}"></script>
-<script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
-<script src="{{ asset('plugins/fullcalendar/fullcalendar.min.js') }}"></script>
-<script src="{{ asset('plugins/vue/vue.js') }}"></script>
-<script src="{{ asset('plugins/axios/axios.js') }}"></script>
-<script src="{{ asset('rsc/js/adminlte.js') }}"></script>
+<script src="http://localhost/votantes/plugins/select2/select2.full.min.js"></script>
+<script src="http://localhost/votantes/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="http://localhost/votantes/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="http://localhost/votantes/plugins/fastclick/fastclick.js"></script>
+<script src="http://localhost/votantes/plugins/toastr/toastr.js"></script>
+<script src="http://localhost/votantes/plugins/sweetalert/sweetalert.min.js"></script>
+<script src="http://localhost/votantes/plugins/moment/moment.min.js"></script>
+<script src="http://localhost/votantes/plugins/fullcalendar/fullcalendar.min.js"></script>
+<script src="http://localhost/votantes/plugins/vue/vue.js"></script>
+<script src="http://localhost/votantes/plugins/axios/axios.js"></script>
+<script src="http://localhost/votantes/rsc/js/adminlte.js"></script>
 
 
 <!--<script src="{{ URL::asset('js/translation.js') }}"></script>-->
-<script src="{{ asset('plugins/chartjs/Chart.min.js') }}"></script>
+<script src="http://localhost/votantes/plugins/chartjs/Chart.min.js"></script>
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
