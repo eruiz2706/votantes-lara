1917ee24e9ba0d9dfa5e7ebb99c70c6097de5c20
eruiz <eruiz2706@gmail.com>
2019-09-08 10:47:06 -0500

    ajuste migracion

--- resources/views/layouts/adminlte/partials/htmlheader.blade.php
+++ resources/views/layouts/adminlte/partials/htmlheader.blade.php
@@ -10,19 +10,19 @@
   <meta name="theme-color" content="#ffffff">
   
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/font-awesome/css/font-awesome.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/ionicons/css/ionicons.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/social/social.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/select2/select2.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/toastr/toastr.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/sweetalert/sweetalert.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/fullcalendar/fullcalendar.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/fullcalendar/fullcalendar.print.css') }}" media="print">
-  <link rel="stylesheet" href="{{ URL::asset('rsc/css/adminlte.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('rsc/css/loader-1.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('rsc/css/style.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('rsc/css/courses.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/font-awesome/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/ionicons/css/ionicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/social/social.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/select2/select2.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/toastr/toastr.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/sweetalert/sweetalert.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar/fullcalendar.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar/fullcalendar.print.css') }}" media="print">
+  <link rel="stylesheet" href="{{ asset('rsc/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('rsc/css/loader-1.css') }}">
+  <link rel="stylesheet" href="{{ asset('rsc/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('rsc/css/courses.css') }}">
 
-  <link rel="stylesheet" href="{{ URL::asset('plugins/morris/morris.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/morris/morris.css') }}">
 </head>
   
--- resources/views/layouts/adminlte/partials/scripts.blade.php
+++ resources/views/layouts/adminlte/partials/scripts.blade.php
@@ -1,24 +1,24 @@
 
-<script src="{{ URL::asset('plugins/jquery/jquery1.9.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/morris/raphael-min.js') }}"></script>
-<script src="{{ URL::asset('plugins/morris/morris.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/morris/morris-horizontal.min.js') }}"></script>
+<script src="{{ asset('plugins/jquery/jquery1.9.min.js') }}"></script>
+<script src="{{ asset('plugins/morris/raphael-min.js') }}"></script>
+<script src="{{ asset('plugins/morris/morris.min.js') }}"></script>
+<script src="{{ asset('plugins/morris/morris-horizontal.min.js') }}"></script>
 
-<script src="{{ URL::asset('plugins/select2/select2.full.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/fastclick/fastclick.js') }}"></script>
-<script src="{{ URL::asset('plugins/toastr/toastr.js') }}"></script>
-<script src="{{ URL::asset('plugins/sweetalert/sweetalert.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/moment/moment.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/fullcalendar/fullcalendar.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/vue/vue.js') }}"></script>
-<script src="{{ URL::asset('plugins/axios/axios.js') }}"></script>
-<script src="{{ URL::asset('rsc/js/adminlte.js') }}"></script>
+<script src="{{ asset('plugins/select2/select2.full.min.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('plugins/fastclick/fastclick.js') }}"></script>
+<script src="{{ asset('plugins/toastr/toastr.js') }}"></script>
+<script src="{{ asset('plugins/sweetalert/sweetalert.min.js') }}"></script>
+<script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('plugins/fullcalendar/fullcalendar.min.js') }}"></script>
+<script src="{{ asset('plugins/vue/vue.js') }}"></script>
+<script src="{{ asset('plugins/axios/axios.js') }}"></script>
+<script src="{{ asset('rsc/js/adminlte.js') }}"></script>
 
 
 <!--<script src="{{ URL::asset('js/translation.js') }}"></script>-->
-<script src="{{ URL::asset('plugins/chartjs/Chart.min.js') }}"></script>
+<script src="{{ asset('plugins/chartjs/Chart.min.js') }}"></script>
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
@@ -35,4 +35,4 @@ var base_url = '<?php echo url('/'); ?>';
 
 
 </script>
-<script src="{{ URL::asset('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
